c35671370c65b8cdfe8cb614a464d40ee5f518c5
Your Name <you@example.com>
2025-03-10 17:09:39 +0400

    Learn Tamil

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Malayalam 
+          Learn Tamil 
         </a>
       </header>
     </div>
